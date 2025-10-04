626bc20db3a7ceecb4f08ae6b3fba6eeb788a4e4
heouii <miraouiilyes55@gmail.com>
2025-10-04 20:26:28 +0200

    g

--- asm07/asm07.s
+++ asm07/asm07.s
@@ -5,43 +5,56 @@ section .text
     global _start
 
 _start:
-    mov rax, [rsp]
+    mov rax, [rsp]          ; argc
     cmp rax, 2
-    jne erreur
+    jne erreur              ; si pas exactement 1 argument utilisateur
 
-    ; MODIF: Supprimé "pop rax" et "pop rsi" qui décalaient la stack
-    mov rsi, [rsp+16]
+    mov rsi, [rsp+16]       ; argv[1] = adresse de la chaîne saisie
     call lire_nombre
-    mov rbx, rax
+    mov rbx, rax            ; sauvegarde du nombre dans rbx
 
     call est_premier
-    ; MODIF: Sauvegarder le résultat dans r15 (vraiment préservé, pas utilisé ailleurs)
-    mov r15, rax
+    mov r15, rax            ; résultat du test (1 = premier, 0 = non-premier)
     mov rdi, rax
 
     call afficher_resultat
     
-    ; MODIF: Utiliser r15 et inverser pour exit code
+    ; exit code inversé (prime → 0, non-prime → 1)
     mov rdi, r15
     xor rdi, 1
     mov rax, 60
     syscall
 
+; -------------------
+; Convertit argv[1] en entier
+; -------------------
 lire_nombre:
-    xor rax, rax
-    xor rcx, rcx
+    xor rax, rax       ; résultat = 0
+    xor rcx, rcx       ; index = 0
 .boucle:
-    mov dl, [rsi+rcx]
-    cmp dl, 0
+    mov bl, [rsi+rcx]  ; lire caractère
+    cmp bl, 0
     je .fin
-    sub dl, '0'
-    imul rax, rax, 10
-    add rax, rdx
+    cmp bl, 10         ; si retour à la ligne
+    je .fin
+    cmp bl, '0'
+    jb erreur          ; caractère non valide
+    cmp bl, '9'
+    ja erreur          ; caractère non valide
+
+    sub bl, '0'        ; convertir en chiffre
+    imul rax, rax, 10  ; résultat *= 10
+    movzx rdx, bl      ; rdx = chiffre
+    add rax, rdx       ; résultat += chiffre
+
     inc rcx
     jmp .boucle
 .fin:
     ret
 
+; -------------------
+; Test si rbx est premier
+; -------------------
 est_premier:
     cmp rbx, 2
     jl .pas_premier
@@ -71,31 +84,32 @@ est_premier:
     jmp .boucle
 
 .est_premier:
-    ; MODIF: Inversé - retourne 1 pour premier (exit code 0 attendu après inversion)
-    mov rax, 1
+    mov rax, 1      ; 1 = premier
     ret
 .pas_premier:
-    ; MODIF: Inversé - retourne 0 pour non-premier (exit code 1 attendu après inversion)
-    mov rax, 0
+    mov rax, 0      ; 0 = non premier
     ret
 
+; -------------------
+; Affiche le résultat (0 ou 1)
+; -------------------
 afficher_resultat:
     add rdi, '0'
     mov [tampon], dil
     
     mov rax, 1
-    push rdi
     mov rdi, 1
     mov rsi, tampon
     mov rdx, 1
     syscall
-    pop rdi
-    ; MODIF: Corriger sub rdi, 0 en sub rdi, '0' pour restaurer la valeur
-    sub rdi, '0'
+    
+    sub rdi, '0'    ; restaurer valeur numérique
     ret
 
+; -------------------
+; Sortie erreur
+; -------------------
 erreur:
     mov rax, 60
-    ; MODIF: Code d'erreur 2 au lieu de 1
     mov rdi, 2
     syscall
